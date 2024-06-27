b883a1a1a3faec04479daa8083e576066f04114f
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-06-27 10:14:01 +0200

    pointer event added :mouse:

--- src/app/components/Projects/index.jsx
+++ src/app/components/Projects/index.jsx
@@ -1,9 +1,24 @@
-
+import React, { useEffect } from 'react';
 import "./style.css"
 
 
-const Projects = () => { 
-    
+const Projects = () => {
+    useEffect(() => {
+        const teaserElements = document.querySelectorAll(".project__teaser");
+        document.body.onpointermove = e => {
+            const { clientX, clientY } = e;
+            teaserElements.forEach(teaser => {
+                teaser.style.left = `${clientX}px`;
+                teaser.style.top = `${clientY}px`;
+            });
+        };
+
+        return () => {
+            document.body.onpointermove = null;
+        };
+    }, []);
+
+
     return (
         <ul className="projects">
             <li className="project">
@@ -20,7 +35,7 @@ const Projects = () => {
                     <span className="project__client">Buurtcampus Oost</span>
                     <span className="project__type">dev</span>
                     <span className="project__year">2023</span>
-                    <img className="project__teaser" src="https://i.pinimg.com/736x/36/3b/66/363b6614dbcf237451ad43e90dd8e8bc.jpg" alt="Description of the image"/>
+                    <img className="project__teaser" src="https://i.pinimg.com/736x/36/3b/66/363b6614dbcf237451ad43e90dd8e8bc.jpg" alt="Description of the image" />
                 </a>
             </li>
             <li className="project">
@@ -29,7 +44,7 @@ const Projects = () => {
                     <span className="project__client">Unc Inc</span>
                     <span className="project__type">dev</span>
                     <span className="project__year">2024</span>
-                    <img className="project__teaser" src="https://i.pinimg.com/736x/7d/2c/4b/7d2c4bb8a1758a5d393746963515e1d1.jpg" alt="Description of the image"/>
+                    <img className="project__teaser" src="https://i.pinimg.com/736x/7d/2c/4b/7d2c4bb8a1758a5d393746963515e1d1.jpg" alt="Description of the image" />
                 </a>
             </li>
             <li className="project">
@@ -38,7 +53,7 @@ const Projects = () => {
                     <span className="project__client"></span>
                     <span className="project__type">dev / design</span>
                     <span className="project__year">2024</span>
-                    <img className="project__teaser" src="https://i.pinimg.com/564x/d4/c4/e9/d4c4e9c85ee27739003ec459b64a89c7.jpg" alt="Description of the image"/>
+                    <img className="project__teaser" src="https://i.pinimg.com/564x/d4/c4/e9/d4c4e9c85ee27739003ec459b64a89c7.jpg" alt="Description of the image" />
                 </a>
             </li>
         </ul>
