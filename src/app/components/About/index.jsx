e14afa0551a798a207ce013466677eaa5880f859
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2024-07-01 20:28:17 +0200

    chancing showcase  :-1:

--- src/app/components/About/index.jsx
+++ src/app/components/About/index.jsx
@@ -6,10 +6,9 @@ const About = () => {
         <section className="about">
             <article className="about__content">
                 <p>Hello! 👋 I'm a frontend development student at the University of Applied Sciences in Amsterdam. 💻</p>
-                <a className="about__link" href="/Contact">Get in contact</a>
+                <div className="link_wrapper"><a className="about__link" href="/Contact">Get in contact</a></div>
             </article>
         </section>
-        
     );
 }
 
--- src/app/components/Showcase/index.jsx
+++ src/app/components/Showcase/index.jsx
@@ -40,20 +40,18 @@ const Showcase = () => {
                 <p>A fresh new look for digital agency focused on a better world</p>
                     <h2>Unc inc</h2>
                 </article>
-                <img className="case__bg" src="https://i.pinimg.com/564x/0d/34/36/0d3436c1e7def4e7fb7bf640af1b0671.jpg" alt="case background" />
                 <img className="case__image" src="https://i.pinimg.com/564x/0d/34/36/0d3436c1e7def4e7fb7bf640af1b0671.jpg" alt="case background" />
                 <img className="case__image__2" src="https://i.pinimg.com/564x/0d/34/36/0d3436c1e7def4e7fb7bf640af1b0671.jpg" alt="case background" />
             </div>
 
-            {/* <div className="case">
+            <div className="case">
                 <article className="case__content">
                     <h2>Bieb in Bloei</h2>
                     <p>Connecting a community through plants</p>
                 </article>
-                <img className="case__bg" src="https://i.pinimg.com/564x/0d/34/36/0d3436c1e7def4e7fb7bf640af1b0671.jpg" alt="case background" />
                 <img className="case__image" src="https://i.pinimg.com/564x/0d/34/36/0d3436c1e7def4e7fb7bf640af1b0671.jpg" alt="case background" />
                 <img className="case__image__2" src="https://i.pinimg.com/564x/0d/34/36/0d3436c1e7def4e7fb7bf640af1b0671.jpg" alt="case background" />
-            </div> */}
+            </div>
         </section>
     );
 }
